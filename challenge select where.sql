e7a69c7f6623ebc9fa7722ef81abc491d357a0cf
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2024-03-20 11:16:05 -0500

    commited on 20th March 2024

---
+++ challenge select where.sql
@@ -0,0 +1,15 @@
+select * from customer;
+
+select email from customer
+where first_name='Nancy' 
+AND last_name='Thomas';
+
+select * from film;
+
+select description from film
+where title='Outlaw Hanky';
+
+select * from address;
+
+select phone from address
+where address='259 Ipoh Drive';
