f904e65637acd3306f7175771194100cf2b68b56
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2024-04-18 17:30:03 -0500

    committed on 18th April 2024

---
+++ 7. Creating Databases and Tables/Primary Key and Foreign Keys.sql
@@ -0,0 +1,29 @@
+--Data Types in PostgreSQL
+--Refer below link for data types available in PostgreSQL
+
+--Primary Keys and Foreign Keys
+--Primary Key:
+--A primary key is a column or a group of columns used to 
+--identify a row uniquely in a table.
+--Example: in dvdrental database in customers table
+--customer_id column is having non-null,unique values as primary key.
+--Primary keys are also important since they allow us
+--to easily discern what columns should be used for
+--joining tables together.
+
+--Foreign Key:
+--A foreign key is a field or group of fields in a table
+--that uniquely identifies a row in another table.
+--A foreign key is defined in a table that references to the primary key
+--of the other table.
+--Recall in the dvdrental database payment table, each 
+--payment row had its unique payment_id (a primary key) and identified the customer
+--that made the payment through the customer_id(a foreign key
+--since it references the customer table's primary key)
+
+--NOTE: Primary Key and Foreign Key typically make good column choices for joining together two or more tables.
+--When creating tables and defining columns, we can use constraints to define columns as being a primary key, or attaching a foreign key relationship to another table.
+
+
+
+
