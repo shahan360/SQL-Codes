6020356e03d959f5df0e8d72f6bf022905d598c4
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2024-05-06 00:39:42 -0500

    committed on 6th May 2024

--- SQL 50 Leetcode/197. Rising Temperature.sql
+++ SQL 50 Leetcode/197. Rising Temperature.sql
@@ -45,15 +45,20 @@ In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
 In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
 """
 -- Write your PostgreSQL query statement below
--- Selecting the Id from the Weather table
-SELECT Weather.Id 
+-- -- Selecting the Id from the Weather table
+-- SELECT Weather.Id 
 
--- Joining the Weather table with itself, creating an alias 'w' for the joined table
-FROM Weather 
-JOIN Weather AS w 
+-- -- Joining the Weather table with itself, creating an alias 'w' for the joined table
+-- FROM Weather 
+-- JOIN Weather AS w 
 
--- Join condition: Matching records where the RecordDate of 'w' is equal to the RecordDate of 'Weather' minus one day
-ON w.RecordDate = Weather.RecordDate - INTERVAL '1 day' 
+-- -- Join condition: Matching records where the RecordDate of 'w' is equal to the RecordDate of 'Weather' minus one day
+-- ON w.RecordDate = Weather.RecordDate - INTERVAL '1 day' 
 
--- Filtering rows where the Temperature of 'Weather' is greater than the Temperature of 'w'
-WHERE Weather.Temperature > w.Temperature;
+-- -- Filtering rows where the Temperature of 'Weather' is greater than the Temperature of 'w'
+-- WHERE Weather.Temperature > w.Temperature;
+
+SELECT w1.id
+FROM Weather w1 LEFT JOIN Weather w2  -- Performing Self Join when we need to join the same table with itself.
+ON w1.recordDate - INTERVAL '1 DAY' = w2.recordDate -- Taking the day interval of 1 day so used INTERVAL '1 DAY'. This is generally used for date operations in SQL.
+WHERE w1.temperature>w2.temperature;
