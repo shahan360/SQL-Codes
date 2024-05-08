5e338afded2935a44a2c3d98621966acddae6e57
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2024-05-08 07:17:07 -0500

    committed on 8th May 2024

---
+++ SQL 50 Leetcode/2356. Number of Unique Subjects Taught by Each Teacher.sql
@@ -0,0 +1,64 @@
+"""
+Table: Teacher
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| teacher_id  | int  |
+| subject_id  | int  |
+| dept_id     | int  |
++-------------+------+
+(subject_id, dept_id) is the primary key (combinations of columns with unique values) of this table.
+Each row in this table indicates that the teacher with teacher_id teaches the subject subject_id in the department dept_id.
+ 
+
+Write a solution to calculate the number of unique subjects each teacher teaches in the university.
+
+Return the result table in any order.
+
+The result format is shown in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Teacher table:
++------------+------------+---------+
+| teacher_id | subject_id | dept_id |
++------------+------------+---------+
+| 1          | 2          | 3       |
+| 1          | 2          | 4       |
+| 1          | 3          | 3       |
+| 2          | 1          | 1       |
+| 2          | 2          | 1       |
+| 2          | 3          | 1       |
+| 2          | 4          | 1       |
++------------+------------+---------+
+Output:  
++------------+-----+
+| teacher_id | cnt |
++------------+-----+
+| 1          | 2   |
+| 2          | 4   |
++------------+-----+
+Explanation: 
+Teacher 1:
+  - They teach subject 2 in departments 3 and 4.
+  - They teach subject 3 in department 3.
+Teacher 2:
+  - They teach subject 1 in department 1.
+  - They teach subject 2 in department 1.
+  - They teach subject 3 in department 1.
+  - They teach subject 4 in department 1.
+"""
+
+-- This SQL query counts the number of distinct subject IDs taught by each teacher.
+
+SELECT 
+    teacher_id, -- Select the teacher_id column
+    COUNT(DISTINCT subject_id) AS cnt -- Count the distinct subject IDs and alias the result as 'cnt'
+FROM 
+    Teacher -- Select from the Teacher table
+GROUP BY 
+    teacher_id; -- Group the results by teacher_id to count the number of distinct subject IDs for each teacher
