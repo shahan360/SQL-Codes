d02bc1d28ba1c1055e6951e8ba08c164cf9c1176
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2024-05-08 08:05:04 -0500

    committed on 8th May 2024

---
+++ SQL 50 Leetcode/1070. Product Sales Analysis III.sql
@@ -0,0 +1,82 @@
+"""
+Table: Sales
+
++-------------+-------+
+| Column Name | Type  |
++-------------+-------+
+| sale_id     | int   |
+| product_id  | int   |
+| year        | int   |
+| quantity    | int   |
+| price       | int   |
++-------------+-------+
+(sale_id, year) is the primary key (combination of columns with unique values) of this table.
+product_id is a foreign key (reference column) to Product table.
+Each row of this table shows a sale on the product product_id in a certain year.
+Note that the price is per unit.
+ 
+
+Table: Product
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| product_id   | int     |
+| product_name | varchar |
++--------------+---------+
+product_id is the primary key (column with unique values) of this table.
+Each row of this table indicates the product name of each product.
+ 
+
+Write a solution to select the product id, year, quantity, and price for the first year of every product sold.
+
+Return the resulting table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Sales table:
++---------+------------+------+----------+-------+
+| sale_id | product_id | year | quantity | price |
++---------+------------+------+----------+-------+ 
+| 1       | 100        | 2008 | 10       | 5000  |
+| 2       | 100        | 2009 | 12       | 5000  |
+| 7       | 200        | 2011 | 15       | 9000  |
++---------+------------+------+----------+-------+
+Product table:
++------------+--------------+
+| product_id | product_name |
++------------+--------------+
+| 100        | Nokia        |
+| 200        | Apple        |
+| 300        | Samsung      |
++------------+--------------+
+Output: 
++------------+------------+----------+-------+
+| product_id | first_year | quantity | price |
++------------+------------+----------+-------+ 
+| 100        | 2008       | 10       | 5000  |
+| 200        | 2011       | 15       | 9000  |
++------------+------------+----------+-------+
+"""
+
+-- Selecting product_id, first_year, quantity, and price
+SELECT s.product_id, T.first_year, s.quantity, s.price
+-- Selecting from the 'Sales' table, aliased as 's'
+FROM Sales s
+-- Performing a left join with a subquery aliased as 'T'
+LEFT JOIN
+-- Subquery to find the first year each product was sold
+(SELECT product_id, MIN(year) AS first_year
+-- Selecting product_id and finding the minimum year for each product
+FROM Sales
+-- Grouping the data by product_id
+GROUP BY product_id) T
+-- Join condition based on product_id
+ON s.product_id = T.product_id
+-- Filtering the results to include only rows where the year matches the first_year
+WHERE s.year = T.first_year;
