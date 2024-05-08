80ca3b707f2c9d328c173f8e329b2d7452e77eb1
Shashank Bhushan <63853143+shahan360@users.noreply.github.com>
2024-05-08 08:18:24 -0500

    committed on 8th May 2024

---
+++ SQL 50 Leetcode/1729. Find Followers Count.sql
@@ -0,0 +1,55 @@
+"""
+Table: Followers
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| user_id     | int  |
+| follower_id | int  |
++-------------+------+
+(user_id, follower_id) is the primary key (combination of columns with unique values) for this table.
+This table contains the IDs of a user and a follower in a social media app where the follower follows the user.
+ 
+
+Write a solution that will, for each user, return the number of followers.
+
+Return the result table ordered by user_id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Followers table:
++---------+-------------+
+| user_id | follower_id |
++---------+-------------+
+| 0       | 1           |
+| 1       | 0           |
+| 2       | 0           |
+| 2       | 1           |
++---------+-------------+
+Output: 
++---------+----------------+
+| user_id | followers_count|
++---------+----------------+
+| 0       | 1              |
+| 1       | 1              |
+| 2       | 2              |
++---------+----------------+
+Explanation: 
+The followers of 0 are {1}
+The followers of 1 are {0}
+The followers of 2 are {0,1}
+"""
+
+-- Selecting user_id and counting the number of follower_ids for each user_id, aliasing the count as 'followers_count'
+SELECT user_id, COUNT(follower_id) AS followers_count
+-- Selecting from the 'followers' table
+FROM followers
+-- Grouping the results by user_id
+GROUP BY user_id
+-- Sorting the results by user_id in ascending order
+ORDER BY user_id ASC;
---
+++ SQL 50 Leetcode/596. Classes More Than 5 Students.sql
@@ -0,0 +1,58 @@
+"""
+Table: Courses
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| student     | varchar |
+| class       | varchar |
++-------------+---------+
+(student, class) is the primary key (combination of columns with unique values) for this table.
+Each row of this table indicates the name of a student and the class in which they are enrolled.
+ 
+
+Write a solution to find all the classes that have at least five students.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Courses table:
++---------+----------+
+| student | class    |
++---------+----------+
+| A       | Math     |
+| B       | English  |
+| C       | Math     |
+| D       | Biology  |
+| E       | Math     |
+| F       | Computer |
+| G       | Math     |
+| H       | Math     |
+| I       | Math     |
++---------+----------+
+Output: 
++---------+
+| class   |
++---------+
+| Math    |
++---------+
+Explanation: 
+- Math has 6 students, so we include it.
+- English has 1 student, so we do not include it.
+- Biology has 1 student, so we do not include it.
+- Computer has 1 student, so we do not include it.
+"""
+
+-- Selecting the 'class' column from the 'Courses' table
+SELECT class
+-- Grouping the results by the 'class' column
+FROM Courses
+GROUP BY class
+-- Filtering the grouped results to include only those with a count of 'student' greater than or equal to 5
+HAVING COUNT(student) >= 5;
