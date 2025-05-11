2ef4065de4e004b92347ee04634f7bf555f16d8a
shahan360 <sbsb4961@gmail.com>
2025-05-11 14:26:19 -0500

    committed on 11th May 2025

---
+++ MySQL_bc/Creating_Database_And_Tables/creating_db.sql
@@ -0,0 +1,7 @@
+-- The general command for creating a database:
+
+CREATE DATABASE <database_name>;
+
+-- A specific example:
+
+CREATE DATABASE soap_store;
---
+++ MySQL_bc/Creating_Database_And_Tables/drop_db.sql
@@ -0,0 +1,7 @@
+-- To drop a database:
+
+DROP DATABASE <database-name>;
+
+-- To use a database:
+
+USE <database-name>;
