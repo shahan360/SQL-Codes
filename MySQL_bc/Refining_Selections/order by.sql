1709a485bce0e35ae64f959587fcac1d6f526219
shahan360 <sbsb4961@gmail.com>
2025-05-16 07:11:34 -0500

    committed on 16th May 2025

--- MySQL_bc/Refining_Selections/order by.sql
+++ MySQL_bc/Refining_Selections/order by.sql
@@ -6,3 +6,14 @@ ORDER BY author_lname DESC;
  
 SELECT * FROM books
 ORDER BY released_year;
+
+
+--- More complex examples of order by
+
+-- Order by the place of columns in the table
+SELECT book_id, author_fname, author_lname, pages
+FROM books ORDER BY 2 desc;
+
+-- Order by multiple columns
+SELECT book_id, author_fname, author_lname, pages
+FROM books ORDER BY author_lname, author_fname;
