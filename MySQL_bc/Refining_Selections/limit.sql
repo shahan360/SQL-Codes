d70e1931294376fcb24a70273f2aa050666b8256
shahan360 <sbsb4961@gmail.com>
2025-05-16 07:21:30 -0500

    committed on 16th May 2025

---
+++ MySQL_bc/Refining_Selections/limit.sql
@@ -0,0 +1,39 @@
+SELECT title FROM books LIMIT 3;
+ 
+SELECT title FROM books LIMIT 1;
+ 
+SELECT title FROM books LIMIT 10;
+ 
+SELECT * FROM books LIMIT 1;
+ 
+SELECT title, released_year FROM books 
+ORDER BY released_year DESC LIMIT 5;
+ 
+SELECT title, released_year FROM books 
+ORDER BY released_year DESC LIMIT 1;
+ 
+SELECT title, released_year FROM books 
+ORDER BY released_year DESC LIMIT 14;
+
+-- this will return the first 5 rows starting from the 0th row
+SELECT title, released_year FROM books 
+ORDER BY released_year DESC LIMIT 0,5;
+ 
+SELECT title, released_year FROM books 
+ORDER BY released_year DESC LIMIT 0,3;
+
+-- this will return the 3 rows starting from the 1st row
+SELECT title, released_year FROM books 
+ORDER BY released_year DESC LIMIT 1,3;
+
+-- this will return the 1 rows starting from the 10th row
+SELECT title, released_year FROM books 
+ORDER BY released_year DESC LIMIT 10,1;
+ 
+SELECT * FROM tbl LIMIT 95,18446744073709551615;
+ 
+SELECT title FROM books LIMIT 5;
+ 
+SELECT title FROM books LIMIT 5, 123219476457;
+ 
+SELECT title FROM books LIMIT 5, 50;
