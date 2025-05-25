52a9cbe592287ea5aa1858768ed1b36beac8cd30
shahan360 <sbsb4961@gmail.com>
2025-05-24 21:58:56 -0500

    committed on 24th May 2025

---
+++ MySQL_bc/Views, Modes and other topics/WITH ROLLUP.sql
@@ -0,0 +1,21 @@
+-- The GROUP BY clause permits a WITH ROLLUP modifier that adds an extra row to the result set that contains the grand total of all the rows in the result set.
+
+SELECT 
+    title, AVG(rating)
+FROM
+    full_reviews
+GROUP BY title WITH ROLLUP;
+ 
+ 
+SELECT 
+    title, COUNT(rating)
+FROM
+    full_reviews
+GROUP BY title WITH ROLLUP;
+ 
+ 
+SELECT 
+    first_name, released_year, genre, AVG(rating)
+FROM
+    full_reviews
+GROUP BY released_year , genre , first_name WITH ROLLUP;
