e58385a943284b9087b67f410aa84a6e4abb6725
shahan360 <sbsb4961@gmail.com>
2025-05-25 23:05:09 -0500

    committed on 25th May 2025

--- MySQL_bc/CAPSTONE PROJECT: Instagram Database Clone/Instagram Database Clone/solutions.sql
+++ MySQL_bc/CAPSTONE PROJECT: Instagram Database Clone/Instagram Database Clone/solutions.sql
@@ -3,3 +3,71 @@
 SELECT * FROM users
 ORDER BY created_at
 LIMIT 5;
+
+-- 2. What day of the week do most users registered on?
+
+SELECT 
+    DAYNAME(created_at) AS day,
+    COUNT(*) AS total
+FROM users
+GROUP BY day
+ORDER BY total DESC
+LIMIT 2;
+
+-- 3. We want to target our inactive users with an email campaign. Find the users who have never posted a photo.
+-- Select users who have never posted a photo
+
+SELECT username
+FROM users
+LEFT JOIN photos
+    ON users.id = photos.user_id
+WHERE photos.id IS NULL;
+
+-- 4. We're running a new contest to see who can get the most likes on a single photo. Who won??
+-- Identify most popular photo (and user who created it)
+
+SELECT 
+    username,
+    photos.id,
+    photos.image_url, 
+    COUNT(*) AS total
+FROM photos
+INNER JOIN likes
+    ON likes.photo_id = photos.id
+INNER JOIN users
+    ON photos.user_id = users.id
+GROUP BY photos.id
+ORDER BY total DESC
+LIMIT 1;
+
+-- 5. Our investors want to know How many times does the average user post?
+-- Calculate average number of posts per user
+
+SELECT (SELECT Count(*) 
+        FROM   photos) / (SELECT Count(*) 
+                          FROM   users) AS avg; 
+
+-- 6. A brand wants to know which hashtags to use in a post.
+-- What are the top 5 most commonly used hashtags?
+
+SELECT tags.tag_name, 
+       Count(*) AS total 
+FROM   photo_tags 
+       JOIN tags 
+         ON photo_tags.tag_id = tags.id 
+GROUP  BY tags.id 
+ORDER  BY total DESC 
+LIMIT  5;
+
+-- 7. We have a small problem with bots on our site...
+-- Find users who have liked every single photo on the site.
+
+SELECT username, 
+       Count(*) AS num_likes 
+FROM   users 
+       INNER JOIN likes 
+               ON users.id = likes.user_id 
+GROUP  BY likes.user_id 
+HAVING num_likes = (SELECT Count(*) 
+                    FROM   photos); 
+
